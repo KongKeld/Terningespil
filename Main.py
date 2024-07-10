cd61773426ab42068acd36dc3b4db714b3905b94
DenGlatteFisk <jonasvegeberg@gmail.com>
2024-07-10 15:16:15 +0200

    Tilføjede loop

--- Main.py
+++ Main.py
@@ -5,14 +5,14 @@
 
 import Terningespil
 i=0
-print("************************************")
-print("* Hej - Velkommen til Terningespil *")
-print("************************************")
+print("**************************************")
+print("** Hej - Velkommen til Terningespil **")
+print("**************************************")
 
 Spillere = []
 
 # Initiering af spillerliste (Kan flyttes ud i en selvstændig fil)
-AntalSpillere = int(input("Hvor mange spillere?"))
+AntalSpillere = int(input("Hvor mange spillere? "))
 for i in range(AntalSpillere):
     navn = input(f"Navn på spiller {i+1}?: ")
     Spillere.append([navn, 0]) 
@@ -20,15 +20,22 @@ for i in range(AntalSpillere):
 # Udskriv listen for at se at den er dannet korrekt
 #bemærk at hvert element i listen er både navn og score
 print(Spillere)
+vinder=False
+runde=1
+while not vinder:
+    print(f"Runde {runde}:")
 
-#Skriv at vi nu starter på runden
-print("Runde 1:")
+    # Spil runde 1 (Mangler at lave en løkke der kører indtil en spiller når max)
+    for i, Person in enumerate(Spillere):
+        Spillere[i][1] = Terningespil.spil()
+        print(f"{i+1}. {Person[0]}: {Person[1]}")
 
-# Spil runde 1 (Mangler at lave en løkke der kører indtil en spiller når max)
-for i, Person in enumerate(Spillere):
-    Spillere[i][1] = Terningespil.spil()
-    print(f"{i+1}. {Person[0]}: {Person[1]}")
-
+        if Spillere[i][1] >= 10:
+            print("Wow nogen har vundet!")
+            vinder=True
+            break
+        else:
+            runde+=1
 
 
 # To forskellige måder at udskrive listen på (Den sidste er pænest)
