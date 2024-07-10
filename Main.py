57feb42d9dce28e176bbaf280252a1f0b64aa4e2
Keld <76216231+KongKeld@users.noreply.github.com>
2024-07-10 09:20:16 +0200

    Lavet liste til at holde styr på poient og kalde terningespil - Mangler løkke til at iterere spil

--- Main.py
+++ Main.py
@@ -4,17 +4,42 @@
 
 
 import Terningespil
-PointBank=0
+i=0
+print("************************************")
+print("* Hej - Velkommen til Terningespil *")
+print("************************************")
+
+Spillere = []
+
+# Initiering af spillerliste (Kan flyttes ud i en selvstændig fil)
+AntalSpillere = int(input("Hvor mange spillere?"))
+for i in range(AntalSpillere):
+    navn = input(f"Navn på spiller {i+1}?: ")
+    Spillere.append([navn, 0]) 
+    
+# Udskriv listen for at se at den er dannet korrekt
+#bemærk at hvert element i listen er både navn og score
+print(Spillere)
+
+#Skriv at vi nu starter på runden
+print("Runde 1:")
+
+# Spil runde 1 (Mangler at lave en løkke der kører indtil en spiller når max)
+for i, Person in enumerate(Spillere):
+    Spillere[i][1] = Terningespil.spil()
+    print(f"{i+1}. {Person[0]}: {Person[1]}")
 
 
-print('Hej')
-#Terningespil.spil()
 
-PointBank+=Terningespil.spil()
+# To forskellige måder at udskrive listen på (Den sidste er pænest)
 
-print("Du har",PointBank,"point i banken")
+# Nr 1    
+print(Spillere)
+
+# Nr 2
+for Spiller in Spillere:
+    print("Spiller:",Spiller[0],", Point:",Spiller[1])
 
 
-print("Spillet er slut")
 
 
